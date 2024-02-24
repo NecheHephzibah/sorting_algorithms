3804b4dc1be8ce6fdccd66ac9891ed5734563f0f
NecheHephzibah <neche.bless@gmail.com>
2024-02-24 14:08:01 -0800

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,6 @@
 #include "sort.h"
 
+void swap_nodes(listint_t **h, listint_t **node1, listint_t *node2);
 
 /**
   * insertion_sort_list - function that sorts a doubly linked list of integers
@@ -8,32 +9,47 @@
   * Return: void
   */
 
-
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *curr;
+	listint_t *current;
+	listint_t *insertionPoint;
+	listint_t *temp;
 
-	if (list == NULL || *list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	curr = (*list)->next;
-	while (curr != NULL)
+	for (current = (*list)->next; current != NULL; current = temp)
 	{
-		temp = curr;
+		temp = current->next;
+		insertionPoint = current->prev;
 
-		while (temp->prev != NULL && temp->prev->n > temp->n)
+		while (insertionPoint != NULL && current->n < insertionPoint->n)
 		{
-			temp->prev->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = temp->prev;
-			temp->next = temp->prev;
-			temp->prev = temp->next->prev;
-			temp->next->prev = temp;
-			if (temp->prev != NULL)
-				temp->prev->next = temp;
-			else
-				*list = temp;
+			swap_nodes(list, &insertionPoint, current);
+			print_list((const listint_t *)*list);
 		}
-		curr = curr->next;
 	}
+}
+
+
+/**
+  * swap_nodes - function that swaps nodes.
+  * @h: the head of the node.
+  * @node1: The first node to swap.
+  * @node2: The second node.
+  */
+
+void swap_nodes(listint_t **h, listint_t **node1, listint_t *node2)
+{
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
+	else
+		*h = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
 }
